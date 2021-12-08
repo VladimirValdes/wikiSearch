75bd3a8b8a386b0a757dcde4106f1bf562c720ed
Vladimir <mendozahum20@gmail.com>
2021-12-07 20:40:01 -0600

    feat: add search method

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { WikiService } from './services/wiki.service';
 
 @Component({
   selector: 'app-search',
@@ -15,9 +17,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private readonly wikiService: WikiService) { }
 
   ngOnInit(): void {
+    this.wikiService.search('angular')
+                    .pipe(
+                      tap( res => console.log( res ))
+                    ).subscribe();
   }
 
 }
--- src/app/pages/search/services/wiki.service.ts
+++ src/app/pages/search/services/wiki.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WikiService {
 
-  constructor() { }
+  constructor( private readonly http: HttpClient) { }
+
+  // 
+  // action=query&format=json&list=search&continue=-%7C%7C&srsearch=Craig%20Noone&sroffset=10
+
+  search( term: string ): Observable<any> {
+
+    const params = {
+      action: 'query',
+      format: 'json',
+      list: 'search',
+      srsearch: term,
+      utf8: '1',
+      origin: '*'
+    }
+
+    return this.http.get< any >(environment.api, { params })
+  }
 }
