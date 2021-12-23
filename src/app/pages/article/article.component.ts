5aae30346671ea657279f262b7c45eaac76c3024
Vladimir <mendozahum20@gmail.com>
2021-12-22 20:22:04 -0600

    feat: add card styles

--- src/app/pages/article/article.component.ts
+++ src/app/pages/article/article.component.ts
@@ -4,11 +4,13 @@ import { Article } from 'src/app/interfaces/wiki.interface';
 @Component({
   selector: 'app-article',
   template: `
-      <article class="article">
-        <a [href]="'https://es.wikipedia.org/?curid='+ article.pageid" target="_blank">
+      <article class="article"
+            (click)="showResult( article.pageid )">
+        <h3 class="article__title"> {{ article.title }}</h3>
+        <!-- <a [href]="'https://es.wikipedia.org/?curid='+ article.pageid" target="_blank">
           {{article.title}}
-        </a>
-        <p [innerHTML]="article.snippet"></p>
+        </a> -->
+        <p [innerHTML]="article.snippet" class="article__desc"></p>
       </article>
   `,
   styleUrls: ['./article.component.scss']
@@ -18,5 +20,8 @@ export class ArticleComponent {
   @Input() article!: Article;
 
 
+  showResult( pageid: number ) {
+    window.open( `https://es.wikipedia.org/?curid=${ pageid }`, "_blank");
+  }
 
 }
